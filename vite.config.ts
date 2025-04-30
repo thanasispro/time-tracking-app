0ea7f4ebe40316ef6082ac359d089b2158aa0a0d
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2025-05-01 00:46:00 +0300

    add gh page support

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'time-tracking-app',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
